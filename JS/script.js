8a575021aa480289926b58744cc8c6280c5d3548
Ananias <amychelleoropezaortiz@gmail.com>
2023-11-30 21:22:24 -0500

    Segundo ejercicio

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
-let numeros = [1,2,3,4,5];
+//EJERCICIO #1
+
+/* let numeros = [1,2,3,4,5];
 
 //Map, función que recorre la lista y te crea una copia.
 let number = numeros.map(number => {return number});
@@ -9,3 +11,15 @@ console.log(number)
 numeros.forEach( numero => {
     console.log(numero)
 })
+ */
+
+//EJERCICIO #2
+
+let numeros = [];
+
+for (let seguidor = 0; seguidor < 20; seguidor++){
+    let numero = parseInt(Math.floor(Math.random()*10) + 1);
+    numeros.push(numero)
+}
+
+console.log(numeros);
